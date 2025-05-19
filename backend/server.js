374fe7b10eaa65080b610826c4b680d9302f60f3
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-05-19 17:07:14 -0300

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Middleware para permitir CORS e métodos como DELETE
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+server.use(router);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("JSON Server is running");
+});
